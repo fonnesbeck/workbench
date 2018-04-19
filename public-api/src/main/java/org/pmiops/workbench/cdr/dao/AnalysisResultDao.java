bf9b3e8f49c4dc567a0b7e4bd700ee79d7968dd9
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-04-19 09:42:37 -0500

    Survey participant count added in survey result object

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/AnalysisResultDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/AnalysisResultDao.java
@@ -13,5 +13,11 @@ public interface AnalysisResultDao extends CrudRepository<AnalysisResult, Long> 
     List<AnalysisResult> findAnalysisResultsByAnalysisIdAndStratum1AndStratum2(Long analysisId, String stratum1, String stratum2);
     AnalysisResult findAnalysisResultByAnalysisId(Long analysisId);
 
+    @Query(nativeQuery=true, value="SELECT ar.stratum_3,ar.stratum_4,ar.stratum_5,ar.count_value from achilles_results ar where (ar.stratum_1=?1 and ar.stratum_2=?2) and ar.analysis_id=?3")
+    List<Object[]> findSurveyAnswers(String surveyConceptId,String questionId,String stratum5);
+
+    @Query(nativeQuery=true, value="SELECT distinct ar.stratum_2,c.concept_name from achilles_results ar join concept c on (ar.stratum_2=cast(c.concept_id as char)) where ar.stratum_1=?1")
+    List<Object[]> findSurveyQuestions(String surveyConceptId);
+
 
 }
--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
@@ -11,4 +11,8 @@ public interface DbDomainDao extends CrudRepository<DbDomain, Long> {
 
     @Query(nativeQuery=true, value="SELECT * from db_domain where db_type='survey' and concept_id <> 0")
     List<DbDomain> findSurveyList();
+
+    @Query(nativeQuery = true,value="SELECT count_value from db_domain where concept_id=?1")
+    Integer findSurveyParticpantCount(Long surveyConceptId);
+
 }

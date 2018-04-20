faa7a949d30337e72f846f83f2367ba801143eb4
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-04-20 16:14:50 -0500

    Changed casting to improve the speed

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/AnalysisResultDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/AnalysisResultDao.java
@@ -22,7 +22,7 @@ public interface AnalysisResultDao extends CrudRepository<AnalysisResult, Long> 
     @Query(nativeQuery=true, value="SELECT ar.stratum_3,ar.stratum_4,ar.stratum_5,ar.count_value from achilles_results ar where (ar.stratum_1=?1 and ar.stratum_2=?2) and ar.analysis_id=?3")
     List<Object[]> findSurveyAnswers(String surveyConceptId,String questionId,String stratum5);
 
-    @Query(nativeQuery=true, value="SELECT distinct ar.stratum_2,c.concept_name from achilles_results ar join concept c on (ar.stratum_2=cast(c.concept_id as char)) where ar.stratum_1=?1")
+    @Query(nativeQuery=true, value="SELECT distinct ar.stratum_2,c.concept_name from achilles_results ar join concept c on (ar.stratum_2=c.concept_id) where ar.stratum_1=?1")
     List<Object[]> findSurveyQuestions(String surveyConceptId);
 
 

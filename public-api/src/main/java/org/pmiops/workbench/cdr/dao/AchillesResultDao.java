02a8b5b6c301d3b2bbb5aaacdbe98d63ae85a951
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-05-10 10:39:05 -0500

    Search API

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
@@ -2,7 +2,14 @@ package org.pmiops.workbench.cdr.dao;
 
 import org.pmiops.workbench.cdr.model.AchillesResult;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.*;
 
 public interface AchillesResultDao extends CrudRepository<AchillesResult, Long> {
     AchillesResult findAchillesResultByAnalysisId(Long analysisId);
+
+
+    @Query(nativeQuery=true,value="select ar.* from achilles_results ar where ar.stratum_1 = ?1 and ar.stratum_2 = ?2 and ar.stratum_4 regexp ?3")
+    List<AchillesResult> findAchillesResults(String surveyConceptId,String qid,String regex);
 }
